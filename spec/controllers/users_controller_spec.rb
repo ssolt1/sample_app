c3a86c60ff2724876d17ce5048cc910e3382930f
Steve Solt <stevesolt@gmail.com>
2016-09-22 21:14:21 -0600

    Modified test script

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,7 +12,7 @@ describe UsersController do
       end
     end
     
-    describe "for all signed-in-users" do
+    describe "for All signed-in-users" do
 
       before(:each) do
         @user = test_sign_in(Factory(:user))
